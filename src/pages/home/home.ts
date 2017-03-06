a44012345b3738ecb303727faec319e4c876fdf6
Jason Smith <s.jason@shaw.ca>
2017-03-05 20:07:17 -0800

    setup google-maps api

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+
+declare var google;
 
 @Component({
   selector: 'page-home',
@@ -8,8 +10,29 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('map') mapElement;
+  map: any;
 
+  constructor(public navCtrl: NavController) {
+    
   }
+
+ionViewDidLoad(){
+  this.initMap();
+}
+
+initMap(){
+
+  let latLng = new google.maps.LatLng(49.265661, -123.250332);
+
+  let mapOptions = {
+    center: latLng,
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+}
 
 }

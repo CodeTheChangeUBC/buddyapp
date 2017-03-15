af0512821042217f51b1e11400a254451d8c00ef
Gurprit Gill <y9c9@ugrad.cs.ubc.ca>
2017-03-14 19:26:42 -0700

    Changed google global variable to be private member

--- src/pages/maps/maps.ts
+++ src/pages/maps/maps.ts
@@ -1,15 +1,15 @@
-import { Component, ViewChild } from '@angular/core';
-import { Geolocation } from 'ionic-native';
-import { NavController } from 'ionic-angular';
-import { TripPage } from '../trip/trip';
+import {Component, ViewChild} from '@angular/core';
+import {Geolocation} from 'ionic-native';
+import {NavController} from 'ionic-angular';
+import {TripPage} from '../trip/trip';
 
-declare var google;
 
 @Component({
   selector: 'page-maps',
   templateUrl: 'maps.html'
 })
 export class Maps {
+  google: any;
 
   @ViewChild('map') mapElement;
   map: any;
@@ -18,60 +18,59 @@ export class Maps {
   constructor(public navCtrl: NavController) {
   }
 
-ionViewDidLoad(){
-  this.initMap();
-}
+  ionViewDidLoad() {
+    this.initMap();
+  }
 
-initMap(){
+  initMap() {
 
-  Geolocation.getCurrentPosition().then((position) => {
+    Geolocation.getCurrentPosition().then((position) => {
 
-  
 
-  let latLng = new google.maps.LatLng(49.265661, -123.250332);
+      let latLng = new this.google.maps.LatLng(49.265661, -123.250332);
 
-  let mapOptions = {
-    center: latLng,
-    zoom: 15,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: this.google.maps.MapTypeId.ROADMAP
+      };
 
-  this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+      this.map = new this.google.maps.Map(this.mapElement.nativeElement, mapOptions);
 
-}, (err) => {
-  console.log(err);
-});
-}
+    }, (err) => {
+      console.log(err);
+    });
+  }
 
-addMarker(){
- 
-  let marker = new google.maps.Marker({
-    map: this.map,
-    animation: google.maps.Animation.DROP,
-    position: this.map.getCenter()
-  });
- 
-  let content = "<h4>Information!</h4>";          
- 
-  this.addInfoWindow(marker, content);
- 
-}
+  addMarker() {
 
-addInfoWindow(marker, content){
- 
-  let infoWindow = new google.maps.InfoWindow({
-    content: content
-  });
- 
-  google.maps.event.addListener(marker, 'click', () => {
-    infoWindow.open(this.map, marker);
-  });
- 
-}
+    let marker = new this.google.maps.Marker({
+      map: this.map,
+      animation: this.google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
 
-/* TODO: Pass in trip destination as an argument */
-selectDestination(destination) {
-  this.navCtrl.push(TripPage, {destination: destination});
-}
+    let content = "<h4>Information!</h4>";
+
+    this.addInfoWindow(marker, content);
+
+  }
+
+  addInfoWindow(marker, content) {
+
+    let infoWindow = new this.google.maps.InfoWindow({
+      content: content
+    });
+
+    this.google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+
+  }
+
+  /* TODO: Pass in trip destination as an argument */
+  selectDestination(destination) {
+    this.navCtrl.push(TripPage, {destination: destination});
+  }
 
 }

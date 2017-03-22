f4ce16293ea8cf33fd1615a64ba01665884f7a77
Jason Smith <s.jason@shaw.ca>
2017-03-22 15:49:38 -0700

    fixed error with map import

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { Maps } from '../maps/maps';
+import { MapPage } from '../map/map';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 
@@ -10,7 +10,7 @@ import { ContactPage } from '../contact/contact';
 export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
-  tab1Root: any = Maps;
+  tab1Root: any = MapPage;
   tab2Root: any = AboutPage;
   tab3Root: any = ContactPage;
 

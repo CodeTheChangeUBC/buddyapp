734398074a526d9933f03ad2933477c4961926fb
Kim <Kim@dhcp-206-87-157-123.ubcsecure.wireless.ubc.ca>
2017-03-21 18:28:33 -0700

    fixed syntax errors, other erros still hapenning though

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
-
 import { TabsPage } from '../pages/tabs/tabs';
 
 

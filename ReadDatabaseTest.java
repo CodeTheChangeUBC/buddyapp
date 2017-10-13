3acfb1b41a7f8fbf0369571094074ce984b8cac4
Justin Derwee-Church <justindc99edu@gmail.com>
2017-10-12 18:28:52 -0700

    Test to read from the database in java
    Requires the postgresql external library.

---
+++ ReadDatabaseTest.java
@@ -0,0 +1,83 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class ReadDatabaseTest {
+
+	public static void main(String[] args) throws Exception {
+		 // Initialize connection variables.
+        String host = "moonwalk-1.postgres.database.azure.com";
+        String database = "postgres";
+        String user = "moonwalk@moonwalk-1";
+        String password = "MichaelJackson1234)(*&";
+
+        // check that the driver is installed
+        try
+        {
+            Class.forName("org.postgresql.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            throw new ClassNotFoundException("PostgreSQL JDBC driver NOT detected in library path.", e);
+        }
+
+        System.out.println("PostgreSQL JDBC driver detected in library path.");
+
+        Connection connection = null;
+
+        // Initialize connection object
+        try
+        {
+            String url = String.format("jdbc:postgresql://%s/%s", host, database);
+
+            // set up the connection properties
+            Properties properties = new Properties();
+            properties.setProperty("user", user);
+            properties.setProperty("password", password);
+            properties.setProperty("ssl", "true");
+
+            // get connection
+            connection = DriverManager.getConnection(url, properties);
+        }
+        catch (SQLException e)
+        {
+            throw new SQLException("Failed to create connection to database.", e);
+        }
+        if (connection != null) 
+        { 
+            System.out.println("Successfully created connection to database.");
+
+            // Perform some SQL queries over the connection.
+            try
+            {
+
+                Statement statement = connection.createStatement();
+                ResultSet results = statement.executeQuery("SELECT * from inventory;");
+                //Replace 'inventory' with any table such as 'public.user'
+                while (results.next())
+                {
+                    String outputString = 
+                        String.format(
+                            "Data row = (%s, %s, %s)",
+                            results.getString(1),
+                            results.getString(2),
+                            results.getString(3));
+                    System.out.println(outputString);
+                }
+            }
+            catch (SQLException e)
+            {
+                throw new SQLException("Encountered an error when executing given sql statement.", e);
+            }       
+        }
+        else {
+            System.out.println("Failed to create connection to database.");
+        }
+        System.out.println("Execution finished.");
+
+	}
+
+}

aef7f96a5417fe21657c1e6ef794e3387c94cf3b
Hannah Lin <hxqlin@gmail.com>
2017-11-16 18:30:40 -0800

    missing scoring function

--- backend/score.js
+++ backend/score.js
@@ -41,5 +41,33 @@ function score(person1, person2) {
 
   if (!gender_match) return Number.NEGATIVE_INFINITY;
 
+  var departure_overlap = ((person1.time_start <= person2.time_end && person2.start_time <= person1.time_end));
+
+  // should we allow people to wait if they want to?
+  if (!departure_overlap) return Number.NEGATIVE_INFINITY;
+
+  var rating_score = 5 - Math.abs(person1.avg_rating - person2.avg_rating);
+
+  var distance_overlap = 0;
+
   return Number.NEGATIVE_INFINITY;
+}
+
+// https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1);
+  var a =
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+  ;
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
 }

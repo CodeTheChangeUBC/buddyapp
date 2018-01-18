a8b385a4dd3621b87bbc3f12b19c0c13d29eed2e
mdnyhus <mdnyhus@gmail.com>
2018-01-18 14:31:07 -0800

    added safewalkHubDict

---
+++ backend/safewalkHubDict.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var safewalkHubDict = [];
+
+safewalkHubDict[1] = {lat: 49.2580479, lon: -123.2530564} // A
+safewalkHubDict[2] = {lat: 49.2617345, lon: -123.2551646} // B
+safewalkHubDict[3] = {lat: 49.2634393, lon: -123.2557118} // C
+safewalkHubDict[4] = {lat: 49.2647381, lon: -123.258673} // D
+safewalkHubDict[5] = {lat: 49.2624276, lon: -123.2390928} // E
+safewalkHubDict[6] = {lat: 49.2636319, lon: -123.2394576} // F
+safewalkHubDict[7] = {lat: 49.2627077, lon: -123.2373762} // G
+safewalkHubDict[8] = {lat: 49.2695057, lon: -123.2496715} // H
+safewalkHubDict[9] = {lat: 49.2711788, lon: -123.2565165} // I
+safewalkHubDict[10] = {lat: 49.2629807, lon: -123.2562268} // J
+safewalkHubDict[11] = {lat: 49.2603131, lon: -123.2531047} // K
+safewalkHubDict[12] = {lat: 49.2588847, lon: -123.2498431} // L
+safewalkHubDict[13] = {lat: 49.2681686, lon: -123.2577932} // M
+safewalkHubDict[14] = {lat: 49.2677065, lon: -123.250519} // N
+safewalkHubDict[15] = {lat: 49.2658093, lon: -123.2481372} // O
+safewalkHubDict[16] = {lat: 49.2684136, lon: -123.2484806} // P
+safewalkHubDict[17] = {lat: 49.2615034, lon: -123.2408309} // Q
+safewalkHubDict[18] = {lat: 49.260075, lon: -123.2396293} // R
+safewalkHubDict[19] = {lat: 49.253941, lon: -123.234973} // S
+safewalkHubDict[20] = {lat: 49.2623016, lon: -123.2445323} // T
+safewalkHubDict[21] = {lat: 49.264059, lon: -123.245337} // U
+safewalkHubDict[22] = {lat: 49.2605722, lon: -123.2488453} // V
+safewalkHubDict[23] = {lat: 49.2649691, lon: -123.2501757} // W
+safewalkHubDict[24] = {lat: 49.2689247, lon: -123.2561517} // X
+safewalkHubDict[25] = {lat: 49.2666704, lon: -123.2550359} // Y
+safewalkHubDict[26] = {lat: 49.2676715, lon: -123.2523644,} // Z
+safewalkHubDict[27] = {lat: 49.2694497, lon: -123.2533085} // a
+safewalkHubDict[28] = {lat: 49.2712278, lon: -123.250519} // b
+
+module.exports = safewalkHubDict;

8c3f2504262c11a5b4b42ac75404e141729dee11
Harry Yao <harryyao13@gmail.com>
2018-01-20 15:35:40 -0800

    Fixed persistent login module syntax

--- backend/controllers/persistent-login-controller.js
+++ backend/controllers/persistent-login-controller.js
@@ -1,60 +1,57 @@
 const client = require('./../server.js');
 
-
-
 // verify persistent login token
 // return true if token valid, false otherwise
 // id is integer, user id
 // token is string, login token
-function verifyToken(id, token) {
+module.exports = {
+	verifyToken: function (id, token) {
+		const query = {
+		  text: 'SELECT auth_token FROM users WHERE id = $1',
+		  values: [id]
+		}
 
-    const query = {
-      text: 'SELECT auth_token FROM users WHERE id = $1',
-      values: [id]
-    }
-
-    client.query(query, (err, res) => {
-      if (err) {
-        console.log(err.stack)
-      } else {
-        if (token.equals(err.auth_token)) {
-          return true;  // TODO verf this true bubbles out
-        } else {
-          return false;
-        }
-      }
-    });
-}
-
-
-// generate random string 20 chars
-function randString() {
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-  for (var i = 0; i < 20; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-  return text;
-}
-
-function replaceJSONToken(String newToken) {
-	client.query('INSERT INTO users(JWT) values($1)',
-		    [newToken], 
-		    function(error,results,fields) {
-			if(error) {
-				console.log("JWT Replace error", error);
-				res.json({
-					status:false,
-					message:'there is an error with JWT insert'
-				})
-			}else {
-				res.json({
-					status:true,
-					data:results,
-					message:'sucessfully replace token'
-				})
+		client.query(query, (err, res) => {
+		  if (err) {
+			console.log(err.stack)
+		  } else {
+			if (token.equals(err.auth_token)) {
+			  return true;  // TODO verf this true bubbles out
+			} else {
+			  return false;
 			}
+		  }
 		});
-	
-}
+	},
+// generate random string 20 chars
+	randString: function() {
+	  var text = "";
+	  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+	  for (var i = 0; i < 20; i++)
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+	  return text;
+	},
+
+	replaceJSONToken: function replaceJSONToken(newToken) {
+		client.query('INSERT INTO users(JWT) values($1)',
+				[newToken], 
+				function(error,results,fields) {
+				if(error) {
+					console.log("JWT Replace error", error);
+					res.json({
+						status:false,
+						message:'there is an error with JWT insert'
+					})
+				}else {
+					res.json({
+						status:true,
+						data:results,
+						message:'sucessfully replace token'
+					})
+				}
+			});
+		
+	}
+};

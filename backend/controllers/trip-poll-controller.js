6c1ba4a3350eee5601149f82c2fad070917b1f88
Nancy Jiang <nancyjiang10@gmail.com>
2018-01-25 20:28:18 -0800

    trip-poll outline

--- backend/controllers/trip-poll-controller.js
+++ backend/controllers/trip-poll-controller.js
@@ -1,3 +1,15 @@
-module.exports.trip-poll=function(userid) {
-    
+/*
+* Check user status
+* Look for value of 0 (pending search)
+*/
+const client = require('./../server.js');
+
+//assume req is a user_id
+module.exports.tripPoll=function(req, res) {
+  var pending = "0";
+  /** DO SOMETHING TO CHECK IF PENDING**/
+  const query = "SEARCH...";
+  client.query(query);
+
+  return pending;
 }

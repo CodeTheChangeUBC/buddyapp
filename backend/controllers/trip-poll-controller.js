cf6739ec37ba6858579829dc293f6e00ced4829b
Nancy Jiang <nancyjiang10@gmail.com>
2018-01-30 13:03:19 -0800

    query string

--- backend/controllers/trip-poll-controller.js
+++ backend/controllers/trip-poll-controller.js
@@ -5,11 +5,15 @@
 const client = require('./../server.js');
 
 //assume req is a user_id
-module.exports.tripPoll=function(req, res) {
-  var pending = "0";
+module.exports.tripPoll=function(id, res) {
+  var pending = false;
   /** DO SOMETHING TO CHECK IF PENDING**/
-  const query = "SEARCH...";
-  client.query(query);
+  const query = `SELECT public.trip.status, public.trip.user_id FROM public.trip WHERE user_id = ${id}`;
 
+  client.query(query);
+  //next steps: we plan on implementing the following
+  //1) send query
+  //2) receive response (the value of status)
+  //3) set pending to TRUE if status is 0
   return pending;
 }

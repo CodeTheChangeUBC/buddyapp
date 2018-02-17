f5aed65256881f397520e6402605cb2ce81561fa
alisawuffles <alisa.46@gmail.com>
2018-02-17 15:31:35 -0800

    added comments and refactored

--- backend/controllers/order-controller.js
+++ backend/controllers/order-controller.js
@@ -36,10 +36,19 @@ users = {
   }
 }
 
+/*
+* return distance between destA and destB
+*/
 function getDistance(destA, destB) {
   return Math.sqrt(Math.pow(destA.lat - destB.lat, 2) + Math.pow(destA.lon - destB.lon, 2))
 }
 
+/*
+* return whether someone can be popped, based on whether there is someone remaining who can walk alone
+* @param remaningUsers: the remaining users
+* @param users: the original users (in order to access canWalkAlone information)
+* @param toPop: the user we want to pop (but aren't sure whether we can)
+*/
 function someoneRemaining(remainingUsers, users, toPop) {
   remaining = false;
   for (var i = 0; i < remainingUsers.length; i++) {
@@ -51,12 +60,19 @@ function someoneRemaining(remainingUsers, users, toPop) {
   return remaining;
 }
 
+/*
+* pops the user from remainingUsers
+*/
 function popUser(remainingUsers, toPop) {
   index = remainingUsers.indexOf(toPop);
   return remainingUsers.splice(index, 1)[0];
 }
 
-
+/*
+* given a starting location, sorts users in the order they should be dropped off
+* @param startLocation: starting location
+* @param users: users in a group
+*/
 function sortDropoffs(startLocation, users) {
     start = startLocation
 
@@ -66,23 +82,27 @@ function sortDropoffs(startLocation, users) {
 
       distances = [];
       for (var i = 0; i < remainingUsers.length; i++) {
-        distances.push([remainingUsers[i], getDistance(start, users[remainingUsers[i]].dest)])
+        distances.push({
+          userID: remainingUsers[i],
+          distance: getDistance(start, users[remainingUsers[i]].dest)
+        })
       }
 
       distances.sort((a, b) => {
-        return a[1] > b[1]
+        return a.distance > b.distance
       });
 
-      if (someoneRemaining(remainingUsers, users, distances[0][0])) {
-        order.push(popUser(remainingUsers, distances[0][0]))
-        start = users[distances[0][0]].dest
+      if (someoneRemaining(remainingUsers, users, distances[0].userID)) {
+        order.push(popUser(remainingUsers, distances[0].userID))
+        start = users[distances[0].userID].dest
       } else {
-        order.push(popUser(remainingUsers, distances[1][0]))
-        start = users[distances[1][0]].dest
+        order.push(popUser(remainingUsers, distances[1].userID))
+        start = users[distances[1].userID].dest
       }
     }
     order.push(remainingUsers[0])
     console.log(order)
 }
 
+/* for testing */
 sortDropoffs(startLocation, users)

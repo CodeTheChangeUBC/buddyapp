cf02610a34a66d1b325658ecb5ff17e8c3bc81b0
alisawuffles <alisa.46@gmail.com>
2018-02-17 19:38:55 -0800

    Jeff fixed it

--- backend/controllers/trip-poll-controller.js
+++ backend/controllers/trip-poll-controller.js
@@ -5,26 +5,32 @@
 const client = require('./../server.js');
 
 //assume req is a user_id
+// searches through trip to check whether a user has been put in a trip that is now pending
 module.exports.tripPoll=function(id, res) {
   var pending = false;
+  // pending = am I in a trip?
   /** DO SOMETHING TO CHECK IF PENDING**/
-  const query = `SELECT public.trip.status, public.trip.user_id FROM public.trip WHERE user_id = ${id}`;
+  const query = `SELECT * FROM public.trip WHERE user_id = ${id} AND status = 0`;
 
-  client.query(query, function (error, results, fields) {
-			if (error) {
-				console.log("error occurred during trip insertion", error);
-				res.json({
-					status: false,
-					message: 'there was some error with insert query'
-				})
-			} else {
-				res.json({
-					status: true,
-					data: results,
-					message: 'trip insertion successful'
-				})
-			}
-	});
+  client.query(query, (err,res) => {
+    if (err) {
+      console.log("error", error);
+      res.json({
+        status: false,
+        message: 'there was some error determining whether user is in trip'
+      })
+    } else {
+      // TODO verify response when no result matching in database
+      if (res.rows[0] != NULL) {
+        pending = true;
+      }
+      res.json({
+        status: true,
+        data: results,
+        message: 'find succcessful'
+      })
+    }
+  })
 
   // we have to set pending somehow
   return pending;

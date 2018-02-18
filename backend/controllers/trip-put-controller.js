60e6a9cb05ae7dba19af5451e731ed216105dae4
Hannah Lin <hxqlin@gmail.com>
2018-02-17 21:58:51 -0800

    minor refactoring

--- backend/controllers/trip-put-controller.js
+++ backend/controllers/trip-put-controller.js
@@ -1,10 +1,10 @@
-var client = require(./../server.js);
+var client = require("./../server.js");
 
 //Function to add a row in the Trip table for every User object in group using a calculated start time.
 module.exports.addTrips = function (group) {
 	var max_start = group[0].time_start;        // max start - latest start time
 	var min_end = group[0].time_end;            // min end - earliest end time
-	const allUserIds = "";
+	var allUserIds = "";
 	for (var i = 0; i < group.length; i++) {
 		var person = group[i];
 		if (person.time_start > max_start) {
@@ -21,7 +21,7 @@ module.exports.addTrips = function (group) {
 
 	//Split the datetime string at these characters
 	var t = min_end.split(/[- :]/);
-	//Pretty sure our Postgres db doesn't have milliseconds so I use 0
+	//Pretty sure our Postgres db doesn"t have milliseconds so I use 0
 	var minEndDate = new Date(Date.UTC(t[0], t[1], t[2], t[3], t[4], t[5], 0));
 	t = max_start.split(/[- :]/);
 	var maxStartDate = new Date(Date.UTC(t[0], t[1], t[2], t[3], t[4], t[5], 0));
@@ -36,6 +36,22 @@ module.exports.addTrips = function (group) {
 	//This is the actual start time that we send the group out at
 	var trueStartTime = new Date(trueStartMS);
 
+	const fnTripInsertion = function (error, results, fields) {
+		if (error) {
+			console.log("error occurred during trip insertion", error);
+			res.json({
+				status: false,
+				message: "there was some error with insert query"
+			})
+		} else {
+			res.json({
+				status: true,
+				data: results,
+				message: "trip insertion successful"
+			})
+		}
+	};
+
 	for (let i = 0; i < group.length; i++) {
 		const person = group[i];
 		const trueStartTimeFormatted = trueStartTime.toISOString();
@@ -45,24 +61,9 @@ module.exports.addTrips = function (group) {
         const status = 0;
 		const userId = person.user_id;
 
-		const query = 'INSERT INTO trip(time_start, time_end, security_code, status, user_ids, user_id) VALUES($1, $2, $3, $4, $5, $6)';
+		const query = "INSERT INTO trip(time_start, time_end, security_code, status, user_ids, user_id) VALUES($1, $2, $3, $4, $5, $6)";
 		const values = [trueStartTimeFormatted, timeEnd, securityCode, status, allUserIds, userId];
 		
-		client.query(query, values,
-			function (error, results, fields) {
-				if (error) {
-					console.log("error occurred during trip insertion", error);
-					res.json({
-						status: false,
-						message: 'there was some error with insert query'
-					})
-				} else {
-					res.json({
-						status: true,
-						data: results,
-						message: 'trip insertion successful'
-					})
-				}
-			});
+		client.query(query, values, fnTripInsertion);
 	}
 }

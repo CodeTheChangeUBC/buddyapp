b845809bd72416ce852a37ce6ccab9a4b0d987a5
NotMuchToLearn <firepower789@gmail.com>
2018-03-10 17:37:01 -0800

    Refactored to use Person objects

--- backend/controllers/trip-put-controller.js
+++ backend/controllers/trip-put-controller.js
@@ -2,28 +2,28 @@ var client = require("./../server.js");
 
 //Function to add a row in the Trip table for every User object in group using a calculated start time.
 module.exports.addTrips = function (group) {
-	var max_start = group[0].time_start;        // max start - latest start time
-	var min_end = group[0].time_end;            // min end - earliest end time
+	var maxStart = group[0].timeStart;        // max start - latest start time
+	var minEnd = group[0].timeEnd;            // min end - earliest end time
 	var allUserIds = "";
 	for (var i = 0; i < group.length; i++) {
 		var person = group[i];
-		if (person.time_start > max_start) {
-			max_start = person.time_start;
+		if (person.timeStart > maxStart) {
+			maxStart = person.timeStart;
 		}
-		if (person.time_end < min_end) {
-			min_end = person.time_end;
+		if (person.timeEnd < minEnd) {
+			minEnd = person.timeEnd;
 		}
 
 		// create the list of all user_ids for the trip as a string
-		allUserIds = allUserIds + person.user_id.toString() + ",";
+		allUserIds = allUserIds + person.userId.toString() + ",";
 	}
 	allUserIds = allUserIds.slice(0, -1);
 
 	//Split the datetime string at these characters
-	var t = min_end.split(/[- :]/);
+	var t = minEnd.split(/[- :]/);
 	//Pretty sure our Postgres db doesn"t have milliseconds so I use 0
 	var minEndDate = new Date(Date.UTC(t[0], t[1], t[2], t[3], t[4], t[5], 0));
-	t = max_start.split(/[- :]/);
+	t = maxStart.split(/[- :]/);
 	var maxStartDate = new Date(Date.UTC(t[0], t[1], t[2], t[3], t[4], t[5], 0));
 
 	//Calculate the average
@@ -55,11 +55,11 @@ module.exports.addTrips = function (group) {
 	for (let i = 0; i < group.length; i++) {
 		const person = group[i];
 		const trueStartTimeFormatted = trueStartTime.toISOString();
-		const timeEnd = person.time_end;
+		const timeEnd = person.timeEnd;
 		// TODO: do we have a proper security code?
 		const securityCode = Math.floor(Math.random() * 10000);
         const status = 0;
-		const userId = person.user_id;
+		const userId = person.userId;
 
 		const query = "INSERT INTO trip(time_start, time_end, security_code, status, user_ids, user_id) VALUES($1, $2, $3, $4, $5, $6)";
 		const values = [trueStartTimeFormatted, timeEnd, securityCode, status, allUserIds, userId];

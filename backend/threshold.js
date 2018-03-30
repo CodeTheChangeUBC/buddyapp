26152183c6a3371ce0633e9e98c8e8222c6c2d07
William Gu <williamgu2011@hotmail.com>
2018-03-29 19:39:13 -0700

    Adjusted scalings for get threshold

--- backend/threshold.js
+++ backend/threshold.js
@@ -1,16 +1,10 @@
 /**
  * Find the minimum threshold rating for matching people, given some total number of people
  * @param  {[Number]} numPeople total number of people
- * @return {[Number]} threshold >= 0 and < 1
+ * @return {[Number]} threshold >= .25 and < .75
  */
 function getThreshhold(numPeople) {
-  /*
-  if (numPeople >= 2 && numPeople < 10)
-  	return 0.2;
-  else if (numPeople >=10 && numPeople < 20)
-  	return 0.3;*/
-  const SCALING_FACT = 10;
-  return numPeople / (numPeople + SCALING_FACT);
+  return 0.5 * numPeople / (numPeople + 10) + 0.25;
 }
 
 module.exports = {
